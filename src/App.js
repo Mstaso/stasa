a28653fed9c70ddcda5906d107ada2e00f4f217f
malcolm <mstaso1@gmail.com>
2021-04-26 15:48:56 -0400

    removed media and search from app

--- src/App.js
+++ src/App.js
@@ -3,106 +3,38 @@ import './App.css';
 import Map from './components/Map'
 import Loader from  './components/Loader'
 import Header from  './components/Header'
-import Media from  './components/Media'
-import Search from  './components/Search'
-
-class App extends React.Component {
-
-  state = {
-    eventData: [],
-    loading: true,
-    searchResults: []
-  }
+import { useState, useEffect } from 'react'
 
 
-  // handleChange = (event) => {
-  //   setSearchTerm(event.target.value);
-  //   fetchMedia();
-  // };
+const App = () => {
+  const [eventData, setEventData] = useState([])
+  const [loading, setLoading] = useState(false)
 
-  fetchEvents = async () => {
-    // setLoading(true)
-    const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events')
-    const { events } = await res.json()
+  useEffect(() => {
 
-    this.setState({
-      eventData: events,
-      loading: true
-    })
-  
-  }
+    const fetchEvents = async () => {
+      setLoading(true)
+      const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events')
+      const { events } = await res.json()
 
-  // useEffect(() => {
-  //   const fetchEvents = async () => {
-  //     setLoading(true)
-  //     const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events')
-  //     const { events } = await res.json()
+      console.log(events)
+      setEventData(events)
+      setLoading(false)
+    }
+    fetchEvents()
+  }, []
+  )
 
-
-  //     setEventData(events)
-  //     setLoading(false)
-  //   }
-  //   fetchEvents()
-  // }, [eventData]
-  // )
-
-  fetchMedia = async (searchTerm) => {
-    console.log(searchTerm)
-    const res = await fetch(`https://images-api.nasa.gov/search?q=${searchTerm}`)
-    const { media } = await res.json()
-    console.log(media);
-    this.setState({searchResults: media})
-  }
-  // useEffect(() => {
-  //   const fetchMedia = async () => {
-  //     const res = await fetch(`https://images-api.nasa.gov/search?q=${searchTerm}`)
-  //     const { media } = await res.json()
-  //     console.log(media);
-  //     setSearchResults(media);
-  //   }
-    
-
-  // }, [searchTerm]
-  // );
-  
-render() {
   return (
     <div>
       <Header />
-      <Search fetchMedia={this.fetchMedia} />
-      <Media />
-      {/* { !loading ? <Map eventData={eventData}/> : <Loader />} */}
+      { !loading ? <Map eventData={eventData}/> : <Loader />}
       
     </div>
   );
 
-}
 
 }
 
 export default App;
 
-
-// const [eventData, setEventData] = useState([])
-//   const [loading, setLoading] = useState(false)
-//   const [searchTerm, setSearchTerm] = useState("");
-//   const [searchResults, setSearchResults] = useState([]);
-
-//   const handleChange = event => {
-//     setSearchTerm(event.target.value);
-//     fetchMedia();
-//   };
-
-//   useEffect(() => {
-//     const fetchEvents = async () => {
-//       setLoading(true)
-//       const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events')
-//       const { events } = await res.json()
-
-
-//       setEventData(events)
-//       setLoading(false)
-//     }
-//     fetchEvents()
-//   }, [eventData]
-//   )
